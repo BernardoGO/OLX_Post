0daaa1f8285099935096f4e4d2a4a9c3908bc63f
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-01-19 01:01:03 -0200

    city fix

--- sel.py
+++ sel.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.support.ui import Select
 
 chromedriver = "./chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
@@ -15,30 +16,31 @@ driver.get("http://minasgerais.olx.com.br/posting.php?pdw=33192&categ_id=832")
 elem = driver.find_element_by_name("title")
 elem.send_keys("smartphone selenium")
 #elem.send_keys(Keys.RETURN)
-time.sleep( 1 )
+
 elem = driver.find_element_by_name("newDescription")
 elem.send_keys("smartphone selenium bem conservado e legal")
-time.sleep( 1 )
+
 elem = driver.find_element_by_name("C")
 elem.send_keys("5000")
-time.sleep( 1 )
+
 elem = driver.find_element_by_name("contact-name")
 elem.send_keys("mauricio")
-time.sleep( 1 )
+
 elem = driver.find_element_by_name("email")
 elem.send_keys("bernardoa2003@yahoo.com.br")
-time.sleep( 1 )
+
 elem = driver.find_element_by_name("state")
 #elem.send_keys("Minas Gera")
-time.sleep( 1 )
-elem = driver.find_element_by_name("region_ddd")
-elem.send_keys("D")
-time.sleep( 1 )
-elem = driver.find_element_by_name("city")
-#elem.send_keys("Belo Horizonte")
-time.sleep( 1 )
+
+select = Select(driver.find_element_by_name("region_ddd"))
+select.select_by_value("25")
+time.sleep(5)
+select = Select(driver.find_element_by_name("city"))
+
+select.select_by_value("52491")
+
 #driver.find_element_by_class_name("olx-ui-button").click();
-time.sleep( 1 )
+
 
 # get performance data
 #performance = driver.execute_script("return window.performance")
